150ded1e008cf3e156a933e3bd9e423d53703ce7
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-10-08 22:54:50 +0700

    add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Logo from '../img/masadam-blog-tranparent.png'
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <img src={Logo} alt=""/>
+      <span>Made with 💙 and <b>React.js</b>.</span>
+    </footer>
   );
 };
 
